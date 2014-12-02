04248fe140dfc76fa6cfe71282b1c453f6aa5b02
Rich Soni <rich@richsoni.com>
2014-12-02 16:11:14 -0500

    add evil

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
       find-file-in-project
       magit
       smex
+      evil
       scpaste
    )
 )
@@ -42,3 +43,19 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 (setq make-backup-files nil)
 (global-set-key "\C-u" 'backward-kill-line)
+
+(evil-mode 1)
+(define-key evil-normal-state-map (kbd ",f") 'projectile-find-file)
+(define-key evil-normal-state-map (kbd ",,") 'evil-buffer)
+(define-key evil-normal-state-map (kbd "q") nil)
+(define-key evil-normal-state-map (kbd "c") nil)
+
+(define-key evil-insert-state-map (kbd "C-e") nil)
+(define-key evil-insert-state-map (kbd "C-d") nil)
+(define-key evil-insert-state-map (kbd "C-k") nil)
+(define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
+(define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+(define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
+
+(define-key evil-motion-state-map (kbd "C-e") nil)
+(define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
--- rich/.#keys.el
+++
@@ -1,1 +0,0 @@
-rich@ElCapitan-7.local.2588

8f996ed915accc5005e79627dfa8601dd78a382b
Rich Soni <rich@richsoni.com>
2014-12-03 21:58:48 -0500

    updates

--- init.el
+++ init.el
@@ -1,4 +1,3 @@
-
 (defun backward-kill-line (arg)
     "Kill ARG lines backward."
       (interactive "p")
@@ -20,6 +19,7 @@
       magit
       smex
       evil
+      smartparens
       scpaste
    )
 )
@@ -45,18 +45,10 @@
 (setq make-backup-files nil)
 (global-set-key "\C-u" 'backward-kill-line)
 
+(smartparens-mode 1)
 (evil-mode 1)
-(define-key evil-normal-state-map (kbd ",f") 'projectile-find-file)
-(define-key evil-normal-state-map (kbd ",,") 'evil-buffer)
-(define-key evil-normal-state-map (kbd "q") nil)
-(define-key evil-normal-state-map (kbd "c") nil)
 
-(define-key evil-insert-state-map (kbd "C-e") nil)
-(define-key evil-insert-state-map (kbd "C-d") nil)
-(define-key evil-insert-state-map (kbd "C-k") nil)
 (define-key evil-insert-state-map (kbd "C-c") 'evil-normal-state)
 (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
 (define-key evil-visual-state-map (kbd "C-c") 'evil-normal-state)
-
-(define-key evil-motion-state-map (kbd "C-e") nil)
 (define-key evil-visual-state-map (kbd "C-c") 'evil-exit-visual-state)
